eef4f2c5d6a01ecabeaeb883bd8539c850ed2d58
ravik2009@outlook.com <ravik2009@outlook.com>
2019-10-11 14:32:02 +0530

    v

--- Base.cs
+++ Base.cs
@@ -15,20 +15,18 @@ namespace Nunitproject_ravi
        public static RemoteWebDriver driver;
 
         [SetUp]
+        [Obsolete]
         public void startBrowser()
         {
 
-              //CHROME and IE            
-            ChromeOptions Options = new ChromeOptions();
-            //InternetExplorerOptions Options = new InternetExplorerOptions();
-            Options.PlatformName = "windows";
-            Options.AddAdditionalCapability("platform", "WIN10", true); // Supported values: "VISTA" (Windows 7), "WIN8" (Windows 8), "WIN8_1" (windows 8.1), "WIN10" (Windows 10), "LINUX" (Linux)
-            Options.AddAdditionalCapability("version", "latest", true); // for Chrome only, you can specify version=latest or the real version number like version=74. For Firefox and IE you must always specify the version number.
-            Options.AddAdditionalCapability("video", "True", true);
+            DesiredCapabilities Capabilities = new DesiredCapabilities();
+            Capabilities.SetCapability(CapabilityType.BrowserName, "Chrome");
 
+            //ChromeOptions Options = new ChromeOptions();
+           
 
             driver = new RemoteWebDriver(
-              new Uri("http://192.168.99.100:4546/wd/hub/"), Options.ToCapabilities(), TimeSpan.FromSeconds(600));// NOTE: connection timeout of 600 seconds or more required for time to launch grid nodes if non are available.
+              new Uri("http://192.168.99.100:4546/wd/hub/"), Capabilities, TimeSpan.FromSeconds(600));// NOTE: connection timeout of 600 seconds or more required for time to launch grid nodes if non are available.
                                                                                                                                           // driver = new ChromeDriver(@"C:\\Users\\Ravi\\.nuget\\packages\\selenium.webdriver.chromedriver\\77.0.3865.4000\\driver\\win32");
         }
 
